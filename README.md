09aad1114fc9a4ef2dc5ce7ce10309d51b8659df
Riyaz Fatkullin <valianros@gmail.com>
2021-01-06 23:34:21 +0300

    added GET method support

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # demoServer
 demo spring server for educational purposes
+
+GET:
+127.0.0.1:8080/plsWork/
+127.0.0.1:8080/secondMethod/
+
+returns simple strings
+
--- src/main/java/tk/pintonda/demoServer/Controller.java
+++ src/main/java/tk/pintonda/demoServer/Controller.java
@@ -1,4 +1,22 @@
 package tk.pintonda.demoServer;
 
-public class Controller {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Controller {
+    @GetMapping("/plsWork")
+    String getMethod(){
+        return "working fine";
+    }
+
+    @GetMapping("/secondMethod")
+    String getMethodTwo(){
+        return "i am still working";
+    }
 }
